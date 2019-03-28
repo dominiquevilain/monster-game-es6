c556360bbfc36324660de475ec52948644d831e9
Dominique Vilain <domi@domy.be>
2019-03-28 09:33:17 +0100

    Add computed property in Opponent

--- src/js/Opponent.js
+++ src/js/Opponent.js
@@ -3,7 +3,7 @@ import { HEALTH_MAX_VALUE } from './settings'
 export default class Opponent {
   constructor (status, name) {
     this.node = null
-    this.status = status
+    this._status = status
     this.name = name
     this._healthValue = HEALTH_MAX_VALUE
   }
@@ -14,5 +14,13 @@ export default class Opponent {
 
   set healthValue (value) {
     this._healthValue = value
+  }
+
+  get status () {
+    return this._status
+  }
+
+  set status (status) {
+    this._status = status
   }
 }
